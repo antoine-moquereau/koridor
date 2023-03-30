be4fb72669866aa24a9bfd2fda2076d7325a6dd5
Antoine Moquereau <25367276+antoine-moquereau@users.noreply.github.com>
2023-03-30 17:48:19 +0200

    Fix: display errors properly

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from '$app/stores'
+</script>
+
+<h1>Oops!</h1>
+<p>Error {$page.status} {$page.error?.message}</p>
+<a href="/">Home</a>
+
+<style>
+  :global(main) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  a {
+    margin-top: 2rem;
+  }
+</style>
