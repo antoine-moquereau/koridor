72f69a261582855d589b0621c61cd4a9432ecd5e
Antoine Moquereau <25367276+antoine-moquereau@users.noreply.github.com>
2023-03-30 19:04:44 +0200

    Fix: use adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-// import adapter from '@sveltejs/adapter-auto'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-cloudflare'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'dist',
-      assets: 'dist',
-      precompress: true
-    })
+    adapter: adapter()
   }
 }
 
